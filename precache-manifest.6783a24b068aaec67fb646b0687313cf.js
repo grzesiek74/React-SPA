e5dde58d08927921b9da3cd610fbc48bc93bcea2
Grzeisek Duliński <grzesiek746@gmail.com>
2021-02-26 17:53:57 +0100

    Updates

---
+++ precache-manifest.6783a24b068aaec67fb646b0687313cf.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "c5d53a0aecac42c0554c",
+    "url": "/React-SPA/static/css/main.1c1ba825.chunk.css"
+  },
+  {
+    "revision": "c5d53a0aecac42c0554c",
+    "url": "/React-SPA/static/js/main.c394c126.chunk.js"
+  },
+  {
+    "revision": "6d97cb180c510d61ae66",
+    "url": "/React-SPA/static/js/runtime~main.b5cc4949.js"
+  },
+  {
+    "revision": "5163c4fe8ecf5ddbb1ec",
+    "url": "/React-SPA/static/js/2.91afb554.chunk.js"
+  },
+  {
+    "revision": "3dc5b4384882b21c35589a404f1c5b77",
+    "url": "/React-SPA/static/media/products.3dc5b438.jpg"
+  },
+  {
+    "revision": "90d98fe37f5ed9e81011b88c9bc1620c",
+    "url": "/React-SPA/static/media/admin.90d98fe3.jpg"
+  },
+  {
+    "revision": "4759f617500d4bfce85aba7f2c6fb34a",
+    "url": "/React-SPA/static/media/home.4759f617.jpg"
+  },
+  {
+    "revision": "143b33e7430a984a05270701b97e55ef",
+    "url": "/React-SPA/static/media/contact.143b33e7.jpg"
+  },
+  {
+    "revision": "06ccbd97e4acf58ae064fc1aa833a4aa",
+    "url": "/React-SPA/static/media/error.06ccbd97.jpg"
+  },
+  {
+    "revision": "f94333d8e6d93f56ea845db742146acc",
+    "url": "/React-SPA/index.html"
+  }
+];
